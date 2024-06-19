c8893749e189427b13b86588385fa1b9863e94de
neryznx <igor.nery4001@gmail.com>
2024-06-19 20:58:27 -0300

    operações

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -2,6 +2,17 @@ public class MinhaClasse {
 
 public static void main(String []args){
     System.out.print(" Olá turma, sejam bem-vindos! ");
+
+    String primeiroNome = "Igor";
+    String segundoNome = "Nery";
+
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+    System.out.println(nomeCompleto);
+   
+}
+
+public static String nomeCompleto(String primeiroNome, String segundoNome){
+    return primeiroNome.concat(" ").concat(segundoNome);
 }
 
 }
